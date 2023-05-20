2a16787a3d81cbb4227970a33d0c6853a812df6f
Whiteivan <beloivanenkodanil@gmail.com>
2023-05-20 15:30:53 +0600

    add func for sort

--- prog.cpp
+++ prog.cpp
@@ -1,25 +1,50 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <vector>
+#include <list>
 
 using std::string;
-using std::vector;
+using std::list;
 using std::ifstream;
 using std::ofstream;
 using std::cin;
 using std::cout;
 using std::cerr;
 
+list<string> SortD(list<string>& dic)
+{
+	for (auto i = dic.begin();i != dic.end();i++)
+	{
+		auto j = i + 1;
+		for (auto a = i.begin(); a!= i.end(); a++)
+		{
+			if (i[a]==j[a])
+			{
+				continue;
+			}
+			else if (i[a]<j[a])
+			{
+				break;
+			} else
+			{
+				string temp = *j;
+				dic.erase(j);
+				dic.insert(i,temp);
+				break;
+			}
+		}
+	}
+	return dic;
+}
+
 int main(int argc, char* argv[])
 {
 	ifstream inFile(argv[1]);
-	vector<string> dataBase;
+	list<string> dataBase;
 	string line;
 	while (getline(inFile, line))
 	{
 		dataBase.push_back(line);
 	}
-
 	return 0;
 }
