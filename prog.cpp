4313273cb64708de2034ed685bcfabefa3f9de7a
Whiteivan <beloivanenkodanil@gmail.com>
2023-05-20 14:21:25 +0600

    prog.cpp add include

--- prog.cpp
+++ prog.cpp
@@ -1,4 +1,15 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector;
+using std::ifstream;
+using std::ofstream;
+using std::cin;
+using std::cout;
+using std::cerr;
 
 int main()
 {
